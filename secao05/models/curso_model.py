30e819328303b386e9f57ef6076f8db35c6e3fc2
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2024-06-21 11:40:20 -0300

    Models

--- secao05/models/curso_model.py
+++ secao05/models/curso_model.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
-from sqlmodel import Fieldm, SQLModel
+from sqlmodel import Field, SQLModel
 
 class CursoModel(SQLModel, table=True):
     __tablename__ : str = 'cursos'
     
     id: Optional[int] = Field(default=None, primary_key= True)
-
+    titulo: str
+    aulas: int
+    horas: int
