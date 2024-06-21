0ef21efddddb680a0b250b99202268c4b9ca2277
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2024-06-21 12:34:29 -0300

    corrigindo o caminho

--- secao05/core/configs.py
+++ secao05/core/configs.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings
 # Definição da classe de configurações usando Pydantic
 class Settings(BaseSettings):
     # Prefixo da versão da API
-    API_V1_STR: str = 'api/v1'
+    API_V1_STR: str = '/api/v1'
     
     # URL de conexão com o banco de dados PostgreSQL usando asyncpg
     DB_URL: str = "postgresql+asyncpg://postgres:rekaue13@localhost:5432/faculdade"
