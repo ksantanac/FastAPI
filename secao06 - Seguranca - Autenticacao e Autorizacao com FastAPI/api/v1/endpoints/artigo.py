0c182dde047e260f55367d76dc05e8d2c0c1a0f3
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2024-06-25 19:09:16 -0300

    api

---
+++ secao06 - Seguranca - Autenticacao e Autorizacao com FastAPI/api/v1/endpoints/artigo.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from fastapi import APIRouter, status, Depends, HTTPException, Response
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from models.artigo_model import ArtigoModel
+from models.usuario_model import UsuarioModel
+from schemas.artigo_schema import ArtigoSchema
+from core.deps import get_session, get_current_user
+
+router = APIRouter()
+
+# POST Artigo
+@router.post("/", response_model=ArtigoSchema, status_code=status.HTTP_201_CREATED)
+async def post_artigo(artigo: ArtigoSchema, usuario_logado: UsuarioModel = Depends(get_current_user), db: AsyncSession = Depends(get_session)):
+    novo_artigo: ArtigoModel = ArtigoModel(titulo=artigo.titulo, descricao=artigo.descricao, url_fonte=artigo.url_fonte, usuario_id=artigo.usuario_id)
+    
+    db.add(novo_artigo)
+    await db.commit()
+    
+    return novo_artigo
